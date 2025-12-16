db3ea8fc2784f51ee833beecb32ad04bf1619c6e
vinicius <vinicesar1111@gmail.com>
2025-12-16 12:27:37 -0300

    criando rota PostTask

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -3,7 +3,7 @@ import z from "zod";
 import { prisma } from "@/lib/db";
 import { getUserIdFromAuthHeader } from "@/lib/auth";
 
-const updateTaskSchema = z.object({
+const CreateTaskSchema = z.object({
   title: z.string().min(1, "Título não pode ser vazio").optional(),
   description: z.string().optional(),
   status: z.enum(["pending", "in_progress", "completed"]).optional(),
@@ -30,6 +30,53 @@ export async function GetTasks(req: Request) {
     return NextResponse.json(
       {
         message: "Erro ao buscar tarefas.",
+        success: false,
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PostTask(req: Request) {
+  try {
+    const authHeader = req.headers.get("Authorization");
+    const userId = getUserIdFromAuthHeader(authHeader);
+
+    const body = await req.json();
+    const parsed = CreateTaskSchema.safeParse(body);
+
+    if (!parsed.success) {
+      return NextResponse.json(
+        {
+          error: parsed.error.flatten().fieldErrors,
+          success: false,
+        },
+        { status: 400 }
+      );
+    }
+
+    const { title, description, status } = parsed.data;
+
+    const task = await prisma.tasks.create({
+      data: {
+        title: title!,
+        description: description || "",
+        status: status,
+        user_id: userId,
+      },
+    });
+
+    return NextResponse.json(
+      {
+        task,
+        success: true,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "Erro ao criar tarefa.",
         success: false,
       },
       { status: 500 }
